0ce67967bbf8d4c42b056b19e573b77686603abf
cl507523 <cl507523@antgroup.com>
2026-01-08 03:53:05 +0000

    fix: log has static mode violation

--- src/compiler/evm_frontend/evm_imported.cpp
+++ src/compiler/evm_frontend/evm_imported.cpp
@@ -51,6 +51,7 @@ inline uint64_t calculateWordCopyGas(uint64_t Size) {
 }
 
 inline void triggerStaticModeViolation(zen::runtime::EVMInstance *Instance) {
+  Instance->setGas(0);
   zen::runtime::EVMInstance::triggerInstanceExceptionOnJIT(
       Instance, zen::common::ErrorCode::EVMStaticModeViolation);
 }
@@ -704,6 +705,10 @@ static void evmEmitLogGeneric(zen::runtime::EVMInstance *Instance,
 
   const evmc_message *Msg = Instance->getCurrentMessage();
   ZEN_ASSERT(Msg && "No current message set in EVMInstance");
+  if (Instance->isStaticMode()) {
+    triggerStaticModeViolation(Instance);
+    return;
+  }
 
   // Calculate required memory size and charge gas
   if (!Instance->expandMemoryChecked(Offset, Size)) {
@@ -773,6 +778,10 @@ const uint8_t *evmHandleCreateInternal(zen::runtime::EVMInstance *Instance,
   ZEN_ASSERT(Msg && "No current message set in EVMInstance");
 
   static thread_local uint8_t ZeroAddress[32] = {0};
+  if (Instance->isStaticMode()) {
+    triggerStaticModeViolation(Instance);
+    return ZeroAddress;
+  }
 
   evmc_revision Rev = Instance->getRevision();
   if (Rev >= EVMC_SHANGHAI && Size > zen::evm::MAX_SIZE_OF_INITCODE) {
@@ -1055,6 +1064,7 @@ void evmHandleInvalid(zen::runtime::EVMInstance *Instance) {
   evmc::Result ExeResult(
       EVMC_INVALID_INSTRUCTION, 0, Instance ? Instance->getGasRefund() : 0,
       Instance->getReturnData().data(), Instance->getReturnData().size());
+  Instance->setGas(0);
   Instance->setExeResult(std::move(ExeResult));
   Instance->exit(4);
 }
@@ -1064,6 +1074,7 @@ void evmHandleUndefined(zen::runtime::EVMInstance *Instance) {
   evmc::Result ExeResult(
       EVMC_UNDEFINED_INSTRUCTION, 0, Instance ? Instance->getGasRefund() : 0,
       Instance->getReturnData().data(), Instance->getReturnData().size());
+  Instance->setGas(0);
   Instance->setExeResult(std::move(ExeResult));
   Instance->exit(5);
 }
