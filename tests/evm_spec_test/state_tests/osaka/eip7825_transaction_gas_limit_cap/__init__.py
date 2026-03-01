bd4e0c1c58c92b118966cb118d0617ab967476c2
ECNUyhy <93872942+ECNUyhy@users.noreply.github.com>
2026-03-01 14:30:12 +0800

    test(evm): add osaka eip7825 modexp gas limit python tests and checklists (#374)

---
+++ tests/evm_spec_test/state_tests/osaka/eip7825_transaction_gas_limit_cap/__init__.py
@@ -0,0 +1,3 @@
+"""
+Tests [EIP-7825: Transaction Gas Limit Cap](https://eips.ethereum.org/EIPS/eip-7825).
+"""
---
+++ tests/evm_spec_test/state_tests/osaka/eip7883_modexp_gas_increase/__init__.py
@@ -0,0 +1,3 @@
+"""
+Tests for [EIP-7883: ModExp Gas Cost Increase](https://eips.ethereum.org/EIPS/eip-7883).
+"""
