28c330a535814fa13488c06ca3930fae4fc4d6b0
ChenLi <cl507523@antgroup.com>
2026-03-02 11:10:31 +0800

    fix(evm): disable SPP gas cost shifting and add opcode validity check in interpreter fallback path (#371)

--- src/evm/evm_cache.cpp
+++ src/evm/evm_cache.cpp
@@ -1117,7 +1117,7 @@ static bool buildGasChunksSPP(const zen::common::Byte *Code, size_t CodeSize,
       continue;
     }
     GasChunkEnd[Blocks[Id].Start] = Blocks[Id].End;
-    GasChunkCost[Blocks[Id].Start] = Metering[Id];
+    GasChunkCost[Blocks[Id].Start] = Blocks[Id].Cost;
   }
 
   return true;
